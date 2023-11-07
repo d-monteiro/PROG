b16ee8b26fc82ce63bd48115310e12e89cb5221d
d-monteiro <149253209+d-monteiro@users.noreply.github.com>
2023-11-07 01:23:40 +0000

    Add files via upload

---
+++ prob7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define TAM 30
+
+void multiEspecial(int n, int v1[], int v2[], int vRet[]){
+    for (int i = 0; i < n; i++){
+        vRet[i]=v1[i]*v2[n-i-1];
+    }
+}
+
+void leVetor(int v[], int n){
+    for(int i=0; i < n; i++){
+        scanf("%d", &v[i]);
+    }
+}
+
+int main()
+{
+    int v1[TAM], v2[TAM], ret[TAM], n;
+    //printf("Quantos elementos? ");
+    scanf("%d", &n);
+
+    //printf("Vetor 1: ");
+    leVetor(v1, n);
+    //printf("Vetor 2: ");
+    leVetor(v2, n);
+    
+      multiEspecial(n, v1, v2, ret);
+    printf("Multiplicacao especial: { ");
+    for(int i=0; i < n; i++){
+        printf("%d ", ret[i]);
+    }
+    printf("}\n");
+    
+    return 0;
+}
