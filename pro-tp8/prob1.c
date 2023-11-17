8fc042dbdadf8e1dd97d52d5e8132193aed25bba
d-monteiro <duartemsm26@gmail.com>
2023-11-17 15:14:00 +0000

    .

--- pro-tp8/prob1.c
+++ pro-tp8/prob1.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 
- int divisao(int dividendo,int divisor,int quociente, int resto){
-    if(divisor==0) return 0;
-    else{
-    int aux=dividendo,c=0;
-        while(aux>0){
+ int divisao(int dividendo,int divisor,int *quociente, int *resto){
+    if(divisor!=0){
+    int c=0,aux=dividendo;
+        while(aux>=divisor){
             aux-=divisor;
             c++;
         }
-        resto=dividendo-(c*divisor);
-        quociente=c;
+        *resto=dividendo-(c * divisor);
+        *quociente=c;
+        return -1;
     }
-    return -1;
+    else return 0;
  }
 
  int main(){
-    int q,r,a;
-    a=divisao(5,2,q,r);
-    printf("quociente:%d\nresto:%d\nreturn:%d\n",q,r,a);
+    int q,r,a;   
+    a=divisao(7,2,&q,&r);
+    printf("Teste 1/4-\nquociente:%d\nresto:%d\nreturn:%d\n\n",q,r,a);
+
+    a=divisao(7,0,&q,&r);
+    printf("Teste 2/4-\nquociente:%d\nresto:%d\nreturn:%d\n\n",q,r,a);
+
+    a=divisao(6,2,&q,&r);
+    printf("Teste 3/4-\nquociente:%d\nresto:%d\nreturn:%d\n\n",q,r,a);
+
+    a=divisao(81937,74,&q,&r);
+    printf("Teste 4/4-\nquociente:%d\nresto:%d\nreturn:%d\n\n",q,r,a);
+
  }
