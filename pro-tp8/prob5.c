d76cf17033a5110f784890bff8d6425e3bd8264a
d-monteiro <duartemsm26@gmail.com>
2023-11-20 21:12:17 +0000

    .

---
+++ pro-tp8/prob5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void contaVezes(int num, int dig,int *ptr_vezes){
+    int v=0;
+    while (num!=0)
+    {
+        int u=num%10;
+        //printf("%d\n",u);
+        if (u==dig) v++;
+        num/=10;
+        //printf("%ld\n",n);
+    }
+    *ptr_vezes=v;
+}
+int main(){
+    int num;
+    int dig;
+    int *vezes;
+    printf("Introduza um número inteiro: ");
+    scanf("%d",&num);
+    printf("Introduza um dígito: ");
+    scanf("%d",&dig);
+    contaVezes(num,dig,&vezes);
+    printf("O digito %d aparece %d vezes no número %d\n",dig,vezes,num);
+}
+
+/*
+num= número introduzido
+dig= dígito pretendido
+v= vezes que aparece o dígito pretendido
+u= último dígito do número
+num%10 isola o último digito
+v++ incrementa o nº de vezes
+num/=10 retira o último dígito após a contagem
+*/
